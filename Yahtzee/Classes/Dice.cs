a127bbc9001148553dc32db331df1dd120754ec8
taiysuzu <74975096+taiysuzu@users.noreply.github.com>
2021-12-14 10:25:30 -0500

    dice class

---
+++ Yahtzee/Classes/Dice.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yahtzee
+{
+    class Dice
+    {
+        int value;
+        Random randGen = new Random();
+
+        Dice(int _value)
+        {
+            value = _value;
+        }
+
+        void Roll()
+        {
+            value = randGen.Next(1, 6);
+        }
+    }
+}
